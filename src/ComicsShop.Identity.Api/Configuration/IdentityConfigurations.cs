e565d3dc7762d5da7327d78874d3101f798adfb8
Daniel Chagas <daniel.boasq@gmail.com>
2024-08-22 18:46:42 -0300

    Initial
    Initial Identity configuration

---
+++ src/ComicsShop.Identity.Api/Configuration/IdentityConfigurations.cs
@@ -0,0 +1,19 @@
+﻿using ComicsShop.Identity.Api.Infrastructure.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace ComicsShop.Identity.Api.Configuration
+{
+    public static class IdentityConfigurations
+    {
+        public static void IdentityConfigure(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddDbContext<ApplicationDbContext>(
+                options => options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"),
+                    r => r.EnableRetryOnFailure(maxRetryCount: 5)));
+
+            builder.Services.AddIdentityApiEndpoints<IdentityUser>()
+                .AddEntityFrameworkStores<ApplicationDbContext>();
+        }
+    }
+}
---
+++ src/ComicsShop.Identity.Api/Program.cs
@@ -0,0 +1,32 @@
+using ComicsShop.Identity.Api.Configuration;
+using Microsoft.AspNetCore.Identity;
+
+var builder = WebApplication.CreateBuilder(args);
+
+#region
+
+builder.IdentityConfigure();
+
+#endregion
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddAuthorization();
+
+var app = builder.Build();
+
+app.MapIdentityApi<IdentityUser>();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.Run();
